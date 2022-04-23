01ae5dd7b4de66228f511d2b3475e6da06b8e27a
912493745 <66937976+912493745@users.noreply.github.com>
2022-04-23 14:19:42 +0800

    Add files via upload

---
+++ url.php
@@ -0,0 +1,6 @@
+ <?php
+ $ip = ($_SERVER["HTTP_VIA"]) ? $_SERVER["HTTP_X_FORWARDED_FOR"] : $_SERVER["REMOTE_ADDR"];
+$ip = ($ip) ? $ip : $_SERVER["REMOTE_ADDR"];
+$txt=$_POST["txt"];
+file_put_contents("url.txt", date( 'Y-m-d H:i:s' ). "\t". "$ip"."\t".$txt .PHP_EOL, FILE_APPEND | LOCK_EX);
+?>
